6a1782952167403c6123f88bd14c4ca40266bc2d
Jean-Marie TRUONG <jean-marie_truong@outlook.fr>
2019-12-02 16:02:40 +0100

    slug

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -9,12 +9,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/wild", name="wild_")
     */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries'
+        ]);
+    }
+
+    /**
+     * @param string $slug The slugger
+     * @Route("/show/{slug}",
+     * requirements={"slug"="[a-z0-9-]+"},
+     * name="show")
+     * @return Response
+    */
+    public function show(string $slug = ''): Response
+    {
+        $title = ucwords(str_replace('-', ' ', $slug));
+        return $this->render('wild/show.html.twig', [
+            'title' => $title
         ]);
     }
 }
