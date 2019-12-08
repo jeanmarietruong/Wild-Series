e0d73df61dd3b9fa9ed347f19309220ea818bb8f
Jean-Marie TRUONG <jean-marie_truong@outlook.fr>
2019-12-08 08:38:40 +0100

    commit

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/wild", name="wild_")
  */
-class WildController extends AbstractController
+ class WildController extends AbstractController
 {
     /**
      * @Route("/", name="index")
